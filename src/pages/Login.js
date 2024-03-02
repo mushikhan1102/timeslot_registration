bfa03340bcb60ec3a46c36265f688c26d8403e5d
mushikhan1102 <mushikhan1102@gmail.com>
2024-03-02 16:16:59 -0500

    pages

---
+++ src/pages/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Login = () => {
+
+return(
+    
+    <h1>Home</h1>
+
+);
+
+};
+
+export default Login;
---
+++ src/pages/Signup.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Signup = () => {
+
+return(
+    
+    <h1>Home</h1>
+
+);
+
+};
+
+export default Signup;
