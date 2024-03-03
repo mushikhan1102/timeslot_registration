fc3d7143c8a5a80dd9418104353787b787d329f8
mushikhan1102 <mushikhan1102@gmail.com>
2024-03-03 14:40:25 -0500

    take out login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -10,7 +9,6 @@ function App() {
 
     <Routes>
       <Route path="/" element={<Home/>} />
-      <Route path="/login" element={<Login/>} />
       <Route path="/signup" element={<Signup/>} />
       
       
